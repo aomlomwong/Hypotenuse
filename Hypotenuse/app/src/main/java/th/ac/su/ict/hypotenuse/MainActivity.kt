d80fe026d356ff74b490d0ef7ce34fcc0f134dfd
aomlomwong <71165296+aomlomwong@users.noreply.github.com>
2021-01-31 11:20:33 +0700

    commit

--- Hypotenuse/app/src/main/java/th/ac/su/ict/hypotenuse/MainActivity.kt
+++ Hypotenuse/app/src/main/java/th/ac/su/ict/hypotenuse/MainActivity.kt
@@ -2,10 +2,34 @@ package th.ac.su.ict.hypotenuse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = findViewById<EditText>(R.id.a)
+        val b = findViewById<EditText>(R.id.b)
+        val total = findViewById<TextView>(R.id.total)
+        val bt = findViewById<TextView>(R.id.solve)
+        bt.setOnClickListener(){
+
+            var aNum:Float = a.text.toString().toFloat()
+            var bNum:Float = b.text.toString().toFloat()
+
+            var totalNum:Float = ((aNum*aNum)+(bNum*bNum))
+            var lastNum: Double = Math.pow
+
+
+
+            total.text =  lastNum.toString()
+
+        }
+
+
+
+
     }
 }
